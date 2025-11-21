6d480595846e54ad6e101413a30e13e6165addd3
OliveADay <sharinggon23@gmail.com>
2025-11-21 10:44:01 -0800

    s

--- consolelike.gd
+++ consolelike.gd
@@ -1,7 +1,36 @@
 extends Label
 
-@export var mainbit = "sigma sigma sigma"
-var words:String
+var mainbit = ""
+var available = false
+var words = []
+@export var cooldown_max = 0.05
+var cooldown_current = 0
+var index = 0
+
+func seperate_by_spaces(str):
+	var words = []
+	var word = ""
+	for char in str:
+		if char==" ":
+			words.append(word)
+			word = ""
+		else:
+			word+=char
+	return words
+	
+func _ready() -> void:
+	mainbit = text
+	text = ""
+	words = seperate_by_spaces(mainbit)
 
 func _process(delta: float) -> void:
-	pass
+	if cooldown_current <= 0 and index<words.size():
+		text += words[index]+" "
+		index+=1
+		cooldown_current = cooldown_max
+	elif cooldown_current > 0:
+		cooldown_current -= delta
+	else:
+		$prompt.position.y = size.y
+		$prompt.visible = true
+			
